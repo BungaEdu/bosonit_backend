64ae5469c6fa35b974f559b72849d3965cb4bc20
BungaEdu <eduardoflores.9595@gmail.com>
2023-04-12 17:04:10 +0200

    estoy a mitad de conseguir hacer los filtros.

--- block1-process-file-and-streams/src/main/java/org/example/Main.java
+++ block1-process-file-and-streams/src/main/java/org/example/Main.java
@@ -2,14 +2,26 @@ package org.example;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class Main {
-    public static void main(String[] args) throws IOException, URISyntaxException {
-        //block1-process-file-and-streams\src\main\resources\people.csv
-        Persona.readFich("block1-process-file-and-streams\\src\\main\\resources\\people.csv");
+    public static <InvalidLineFormatException extends Throwable> void main(String[] args) throws IOException, URISyntaxException, Persona.InvalidLineFormatException {
+        System.out.println("hello world");
+        //La ruta a utilizar es: "block1-process-file-and-streams\src\main\resources\people.csv"
+        //TODO lectura por pantalla con scanner seguramente
+        //
+        //
+
+        String route = "block1-process-file-and-streams\\src\\main\\resources\\people.csv";
+
+        ArrayList resultado = Persona.createPersonList(Persona.readFichAllToList(route));
+        for (Object p : resultado) {
+            System.out.println(p);
+        }
+
 
     }
+
 }
--- src/apuntes.java
+++ src/apuntes.java
@@ -1,12 +1,12 @@
+/*
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class apuntes {
     public static void main(String[] args) {
-        List<String> lista = List.of("Hola", "", "Java", "", "Concurrente", "Streams", "", "Java11", "Java12");
-        Stream<String> stream = lista.stream(); //Convertimos la lista
-        long conteo = stream.filter(perro -> perro.equals(""))
-                .count();
-        System.out.println(conteo); //5
+        ArrayList<Persona> listPeople = new ArrayList<Persona>();
+        listPeople.put("perro", "eded", "")
     }
 }
+*/
