f96010fa601730f7f81933d6c58b7a62e49df844
BungaEdu <eduardoflores.9595@gmail.com>
2023-04-17 16:53:27 +0200

    primer commit block5

--- block5-command-line-runner/src/main/java/com/example/block5commandlinerunner/Block5CommandLineRunnerApplication.java
+++ block5-command-line-runner/src/main/java/com/example/block5commandlinerunner/Block5CommandLineRunnerApplication.java
@@ -1,13 +1,38 @@
 package com.example.block5commandlinerunner;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Block5CommandLineRunnerApplication {
+public class Block5CommandLineRunnerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Block5CommandLineRunnerApplication.class, args);
 	}
+
+	@PostConstruct
+	public void primerMensaje () {
+		System.out.println("Hola desde clase inicial");
+	}
+
+	public void segundoMensaje () {
+		System.out.println("Hola desde clase secundaria");
+	}
+
+	public void tercerMensaje () {
+		System.out.println("Soy la tercera clase");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		segundoMensaje();
+		tercerMensaje();
+	}
+
+	/*Los mensajes se muestran dependiendo del orden
+	de ejecución que le ponga en el run al sobreescribirlo
+	 */
 
 }
