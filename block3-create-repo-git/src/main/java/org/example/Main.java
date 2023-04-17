a9ca87f0f562c7884985a960fee768d5c0289c91
BungaEdu <eduardoflores.9595@gmail.com>
2023-04-17 16:29:45 +0200

    primer commit block5

--- block3-create-repo-git/src/main/java/org/example/Main.java
+++ block3-create-repo-git/src/main/java/org/example/Main.java
@@ -1,22 +1,13 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-        /*
-        Hacer un clon desde el repositorio de github en otro directorio de nuestro ordenador. Le llamaremos ‘COPIA PROYECTOGIT”.
-Modificar el fichero1 añadiendo la línea “Segunda línea de fichero1”
-Realizar un commit y push.
-
-me quedo aquí
+        /*DIRECTORIO CORRECTO:
+        https://github.com/BungaEdu/bosonit_PROYECTOGIT
+        */
 
 
-
-         */
     }
 }
