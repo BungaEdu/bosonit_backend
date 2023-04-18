0fea28ed6a6bd6069500caf4725e1ff904142f3e
BungaEdu <eduardoflores.9595@gmail.com>
2023-04-18 13:56:21 +0200

    block5 - ejercicio2 yaml terminado y mejorado.

--- block5-properties/src/main/java/com/example/block5properties/Block5PropertiesApplication.java
+++ block5-properties/src/main/java/com/example/block5properties/Block5PropertiesApplication.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
 public class Block5PropertiesApplication implements CommandLineRunner {
@@ -34,5 +37,4 @@ public class Block5PropertiesApplication implements CommandLineRunner {
 		this.prueba = enviroment.getProperty("greeting");
 		System.out.println(prueba);
 	}
-
 }
