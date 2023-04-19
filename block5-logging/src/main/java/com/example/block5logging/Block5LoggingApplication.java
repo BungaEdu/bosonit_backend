97f1ef8f777fdae97d0df8cb075271ed10d2bb62
BungaEdu <eduardoflores.9595@gmail.com>
2023-04-19 13:19:59 +0200

    block 5 - log: a medias, consigo que se guarde error, pero no consigo que el resto se muestre por consola

--- block5-logging/src/main/java/com/example/block5logging/Block5LoggingApplication.java
+++ block5-logging/src/main/java/com/example/block5logging/Block5LoggingApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Block5LoggingApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(Block5LoggingApplication.class, args);
-	}
-
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Block5LoggingApplication.class, args);
+    }
 }
+
--- block5-logging/src/main/java/com/example/block5logging/Controller.java
+++ block5-logging/src/main/java/com/example/block5logging/Controller.java
@@ -2,24 +2,20 @@ package com.example.block5logging;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    Logger log = LoggerFactory.getLogger(Controller.class);
+    @RequestMapping("/")
+    public String logs(){
+        log.trace("Mensaje TRACE");
+        log.debug("Mensaje DEBUG");
+        log.info("Mensaje INFO");
+        log.warn("Mensaje WARNING");
+        log.error("Mensaje ERROR");
 
-    Logger logger = LoggerFactory.getLogger(Controller.class);
-
-
-    @Autowired
-    public String index() {
-        logger.trace("Mensaje a nivel de TRACE");
-        logger.debug("Mensaje a nivel de DEBUG");
-        logger.info("Mensaje a nivel de INFO");
-        logger.warn("Mensaje a nivel de WARNING");
-        logger.error("Mensaje a nivel de ERROR");
-
-        return "Hola! Mira los logs para ver los resultados";
+        return "Ejercicio bloque 5 logging. Mira los logs";
     }
 }
