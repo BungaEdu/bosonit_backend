4a50ec048694769f0289b6905fa160dd4ba73283
BungaEdu <eduardoflores.9595@gmail.com>
2023-06-13 19:26:13 +0200

    Block12Mongo: actualizo proyecto con solo Persona

--- block12-crud-validation-mongo/src/main/java/com/example/block7crudvalidation_v2/exceptions/UnprocessableEntityException.java
+++
@@ -1,7 +0,0 @@
-package com.example.block7crudvalidation_v2.exceptions;
-
-public class UnprocessableEntityException extends RuntimeException{
-    public UnprocessableEntityException(String message) {
-        super(message);
-    }
-}
---
+++ block12-mongo/src/main/java/com/example/block7crud/domain/Persona.java
@@ -0,0 +1,37 @@
+package com.example.block7crud.domain;
+import com.example.block7crud.controller.dto.PersonaInputDto;
+import com.example.block7crud.controller.dto.PersonaOutputDto;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Persona {
+    @Id
+    @GeneratedValue
+    int id;
+    String nombre;
+    int edad;
+    String poblacion;
+
+    public Persona(PersonaInputDto personaInputDto) {
+        this.nombre = personaInputDto.getNombre();
+        this.edad = personaInputDto.getEdad();
+        this.poblacion = personaInputDto.getPoblacion();
+    }
+
+    public PersonaOutputDto personaToPersonaOutputDto() {
+        return new PersonaOutputDto(
+                this.id,
+                this.nombre,
+                this.edad,
+                this.poblacion
+        );
+    }
+}
